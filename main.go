99eb24d4308fa185cbe252acf4ccce15bdf849de
Ahmet Sari <106359636+eeeng@users.noreply.github.com>
2022-12-05 23:54:49 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,96 @@
+//package main
+
+//import "fmt"
+
+//func main() {
+//	fmt.Print("HEY")
+//	fmt.Println("BERRE")
+//}
+
+// package main
+
+// import (
+// 	"net/http"
+// )
+
+// func aboutHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Write([]byte("About Page"))
+// }
+
+// func main() {
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		w.Write([]byte("Merhaba Berre"))
+// 	})
+
+//		http.HandleFunc("/about", aboutHandler)
+//		http.ListenAndServe(":8080", nil)
+//	}
+package main
+
+import (
+	"encoding/json"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+
+	model "./models"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":9000", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	page := model.Page{ID: 3, Name: "Users", Description: "Users List", URI: "/users"}
+	users := loadUsers()
+	interests := loadInterests()
+	interestMappings := loadInterestMappings()
+
+	var newUsers []model.User
+
+	for _, user := range users {
+		for _, interestMapping := range interestMappings {
+			if user.ID == interestMapping.UserID {
+				for _, interest := range interests {
+					if interestMapping.InterestID == interest.ID {
+						user.Interests = append(user.Interests, interest)
+					}
+				}
+			}
+		}
+		newUsers = append(newUsers, user)
+	}
+	viewModel := model.UserViewModel{Page: page, Users: newUsers}
+	t, _ := template.ParseFiles("template/page.html")
+	t.Execute(w, viewModel)
+}
+
+func loadFile(fileName string) (string, error) {
+	bytes, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func loadUsers() []model.User {
+	bytes, _ := ioutil.ReadFile("json/users.json")
+	var users []model.User
+	json.Unmarshal(bytes, &users)
+	return users
+}
+
+func loadInterests() []model.Interest {
+	bytes, _ := ioutil.ReadFile("json/interests.json")
+	var interests []model.Interest
+	json.Unmarshal(bytes, &interests)
+	return interests
+}
+
+func loadInterestMappings() []model.InterestMapping {
+	bytes, _ := ioutil.ReadFile("json/userInterestMappings.json")
+	var InterestMappings []model.InterestMapping
+	json.Unmarshal(bytes, &InterestMappings)
+	return InterestMappings
+}
